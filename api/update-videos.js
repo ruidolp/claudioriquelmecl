d564c58b9489d878880fdef1c652eddf53e2a2f4
Claudio Riquelme <ruido@sol.local>
2025-09-29 12:19:25 -0300

    El yutu10

--- api/update-videos.js
+++ api/update-videos.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         body: JSON.stringify({
           items: [
             {
-              operation: 'update',
+              operation: 'upsert',
               key: 'videos',
               value: { videos: videos }
             }
